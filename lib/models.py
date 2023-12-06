79f3e6b12c7b37e96b4b1e88e5ed9f8929a12327
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-06 18:36:32 +0300

    completed

--- lib/models.py
+++ lib/models.py
@@ -1,15 +1,5 @@
-import os
-import sys
-
-sys.path.append(os.getcwd())
-
-
 from datetime import datetime
-
-from sqlalchemy import create_engine, desc
-from sqlalchemy import (CheckConstraint, UniqueConstraint,
-    Column, DateTime, Integer, String)
-
+from sqlalchemy import create_engine, desc, Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///migrations_test.db')
@@ -18,9 +8,8 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
-
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), index=True)
+    full_name = Column(String(), index=True)
     email = Column(String(55))
     grade = Column(Integer())
     birthday = Column(DateTime())
@@ -28,5 +17,5 @@ class Student(Base):
 
     def __repr__(self):
         return f"Student {self.id}: " \
-            + f"{self.name}, " \
+            + f"{self.full_name}, " \
             + f"Grade {self.grade}"
